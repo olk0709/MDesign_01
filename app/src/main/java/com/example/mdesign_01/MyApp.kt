b87a19b64e8342b90759fb8ad12c653b104e8357
olk0709 <olk0709@gmail.com>
2023-04-05 16:14:22 +0300

    MDesign.Начало

--- app/src/main/java/com/example/mdesign_01/MyApp.kt
+++ app/src/main/java/com/example/mdesign_01/MyApp.kt
@@ -6,6 +6,7 @@ import com.google.android.material.color.DynamicColors
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
